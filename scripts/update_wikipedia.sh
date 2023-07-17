ecb58f39640a68057eb800fd8476e868e02e8fdf
Maksim Lin <maks@manichord.com>
2023-07-17 14:20:49 +1000

    on MacOS sed i flag needs suffix

--- scripts/update_wikipedia.sh
+++ scripts/update_wikipedia.sh
@@ -2,6 +2,6 @@
 
 ls -l README.md
 
-sed -i "/^Building Wikipedia Project/s=[0-9]\{1,2\}m.[0-9]\{1,2\}[s]\$=$BUILD_TIME=" README.md
+sed -i "" "/^Building Wikipedia Project/s=[0-9]\{1,2\}m.[0-9]\{1,2\}[s]\$=$BUILD_TIME=" README.md
 
-sed -i "/^Running Wikipedia Tests/s=[0-9]\{1,2\}m.[0-9]\{1,2\}[s]\$=$TEST_TIME=" README.md
+sed -i "" "/^Running Wikipedia Tests/s=[0-9]\{1,2\}m.[0-9]\{1,2\}[s]\$=$TEST_TIME=" README.md
